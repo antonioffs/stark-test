16a5f17e4fd204f25aae05b86034f4181ba7706c
Antonio Silva <antonio.ffsmail@gmail.com>
2026-07-11 17:15:33 -0300

    Added faker

---
+++ starkbank_app/fake.py
@@ -0,0 +1,17 @@
+from faker import Faker
+from validate_docbr import CPF
+
+_fake = Faker('pt_BR')
+_cpf = CPF()
+
+
+def generate_cpf():
+    return _cpf.generate()
+
+
+def is_valid_cpf(document):
+    return _cpf.validate(document)
+
+
+def generate_fullname():
+    return _fake.name()
