7926acef3e0b38c3d37ee510ae70845469039e8b
Antonio Silva <antonio.ffsmail@gmail.com>
2026-07-12 14:57:08 -0300

    Created WebhookInvoiceEvent

--- starkbank_app/models.py
+++ starkbank_app/models.py
@@ -1,6 +1,9 @@
+import logging
 from uuid import uuid4
 
 from django.db import models
+
+logger = logging.getLogger(__name__)
 
 
 class Customer(models.Model):
@@ -34,3 +37,26 @@ class Invoice(models.Model):
     def amount_display(self):
         return f'R$ {self.amount / 100:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.')
     amount_display.short_description = 'Amount'
+
+    @staticmethod
+    def mark_invoice_as_paid(gateway_reference_id):
+        updated = Invoice.objects.filter(
+            gateway_reference_id=gateway_reference_id,
+        ).exclude(status=Invoice.Status.PAID).update(status=Invoice.Status.PAID)
+        if updated:
+            logger.info(
+                f'starkbank_webhook: invoice {gateway_reference_id} updated'
+            )
+        else:
+            logger.warning(
+                f'starkbank_webhook: no local invoice found for gateway_reference_id={gateway_reference_id}'
+            )
+
+
+class WebhookInvoiceEvent(models.Model):
+    event_id = models.CharField(max_length=50, unique=True)
+    gateway_reference_id = models.CharField(max_length=50, null=True, blank=True)
+    received_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.event_id
